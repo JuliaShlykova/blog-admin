56f5b2f1b5cd61ee99eb3267f0e1dc6b6a08aceb
Julia Shlykova <julia.shlykova17@gmail.com>
2023-06-21 12:00:00 +0300

    add url api

--- src/apiCaller.js
+++ src/apiCaller.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000';
+const url = 'https://blog-api-xkrg.onrender.com';
 
 const getConfig = () => {
   const token = localStorage.getItem('token');
